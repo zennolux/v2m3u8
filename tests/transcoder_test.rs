802adf0512b5f881c20f92c21e53d65cf7a40959
zennolux <zennolux@gmail.com>
2024-09-22 17:10:19 +0800

    chore: add unit test for listening progress

--- tests/transcoder_test.rs
+++ tests/transcoder_test.rs
@@ -1,3 +1,5 @@
+use std::thread;
+
 use v2m3u8::transcoder::Transcoder;
 
 #[test]
@@ -11,4 +13,22 @@ fn it_can_transcode() {
     };
 
     assert!(result);
+}
+
+#[test]
+fn it_can_listen_progress() {
+    let mut transcoder =
+        Transcoder::new("/tmp/mp4/daolang.mp4".to_string(), "/tmp/m3u8".to_string());
+
+    let data = transcoder.clone();
+    let handler = thread::spawn(move || data.to_m3u8().unwrap());
+
+    let listener = thread::spawn(move || {
+        transcoder.listen_progress(|this| {
+            println!("{}", this.progress);
+        });
+    });
+
+    handler.join().unwrap();
+    listener.join().unwrap();
 }
